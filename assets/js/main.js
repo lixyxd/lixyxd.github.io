1ed20ce6343b26bfc315c78d0a8740128e2499e7
Xiaoyong Li <lixiaoyong@xidian.edu.cn>
2026-08-15 12:14:46 +0800

    左上角信息条优化：玻璃面板两行布局，日期+农历合并一行，时间+天气一行，移动端隐藏日期行

--- assets/js/main.js
+++ assets/js/main.js
@@ -126,7 +126,6 @@
 
   /* ---- nav clock + lunar + weather (IP-based) ---- */
   var navDate = document.getElementById('navDate');
-  var navLunar = document.getElementById('navLunar');
   var navTime = document.getElementById('navTime');
   var navWeather = document.getElementById('navWeather');
   var pad2 = function (n) { return (n < 10 ? '0' : '') + n; };
@@ -190,10 +189,10 @@
   };
   var tickClock = function () {
     var d = new Date();
-    if (navDate) navDate.textContent = '公元' + d.getFullYear() + '年' + (d.getMonth() + 1) + '月' + d.getDate() + '日 周' + WEEK[d.getDay()];
-    if (navLunar) {
+    if (navDate) {
       var l = solar2lunar(d.getFullYear(), d.getMonth() + 1, d.getDate());
-      navLunar.textContent = '农历' + l.gan + l.zhi + '年' + (l.isLeap ? '闰' : '') + MONTH_CN[l.month - 1] + DAY_CN[l.day - 1];
+      navDate.textContent = '公元' + d.getFullYear() + '年' + (d.getMonth() + 1) + '月' + d.getDate() + '日 周' + WEEK[d.getDay()] +
+        ' · 农历' + l.gan + l.zhi + '年' + (l.isLeap ? '闰' : '') + MONTH_CN[l.month - 1] + DAY_CN[l.day - 1];
     }
     if (navTime) navTime.textContent = pad2(d.getHours()) + ':' + pad2(d.getMinutes()) + ':' + pad2(d.getSeconds());
   };
